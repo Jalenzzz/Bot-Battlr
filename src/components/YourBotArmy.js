c5ed73553f1ea3c1a2d1401d51e441a3993d1be8
Jalen <mnenejalen72@gmail.com>
2024-05-10 01:59:49 +0300

    Added components to my bot army

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,14 +1,18 @@
 import React from "react";
 import BotCard from "./BotCard";
+
+// Component for rendering the user's bot army
 function YourBotArmy({ bots, action, removeCard }) {
-  //your bot army code here...
+  // Render the bot army
 
   return (
     <div className="ui segment inverted olive bot-army">
+      {/* Grid layout for bot army */}
       <div className="ui five column grid">
         <div className="row bot-army-row">
-          {/*...and here...*/}
+          {/* Title for the bot army */}
           Your Bot Army
+          {/* Map over each bot in the army and render a BotCard */}
           {bots &&
             bots.map((bot) => {
               return (
