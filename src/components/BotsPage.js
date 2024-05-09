650adb4805a4a096f2d3897d11309388c25a7879
Jalen <mnenejalen72@gmail.com>
2024-05-10 01:48:40 +0300

    Added comments for the bot page

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -3,6 +3,7 @@ import BotCollection from "./BotCollection";
 import YourBotArmy from "./YourBotArmy";
 import BotSpecs from "./BotSpecs";
 
+// Component for managing the bot page
 function BotsPage() {
   const [botCollection, setBotCollection] = useState([]);
   const [filteredCollection, setFilteredCollection] = useState([]);
@@ -10,6 +11,7 @@ function BotsPage() {
   const [collectionVisible, setCollectionVisible] = useState(true);
   const [botSpecs, setBotSpecs] = useState({});
 
+  // Fetch bot data from the database
   useEffect(() => {
     fetch("http://localhost:8002/bots")
       .then((res) => res.json())
@@ -19,6 +21,7 @@ function BotsPage() {
       });
   }, []);
 
+  // Add a bot to the army
   function addToArmy(bot) {
     const newCollection = filteredCollection.filter(
       (card) => card.bot_class !== bot.bot_class
@@ -28,6 +31,7 @@ function BotsPage() {
     setCollectionVisible(true);
   }
 
+  // Remove a bot from the army
   function removeFromArmy(bot) {
     const newArmy = botArmy.filter((card) => card.id !== bot.id);
     const armyClasses = newArmy.map((bot) => bot.bot_class);
@@ -38,6 +42,7 @@ function BotsPage() {
     setFilteredCollection(newCollection);
   }
 
+  // Delete a bot from the collection
   function deleteBot(bot) {
     const newCollection = botCollection.filter((card) => card !== bot);
     const newFilteredCollection = filteredCollection.filter(
@@ -48,6 +53,7 @@ function BotsPage() {
     setFilteredCollection(newFilteredCollection);
     setBotArmy(newArmy);
 
+    // Send a DELETE request to the server to delete the bot
     fetch(`http://localhost:8002/bots/${bot.id}`, {
       method: "DELETE",
     })
@@ -55,15 +61,18 @@ function BotsPage() {
       .then((data) => alert("The selected bot has been deleted successfully!"));
   }
 
+  // Display bot specifications
   function displayBotSpecs(bot) {
     setCollectionVisible(false);
     setBotSpecs(bot);
   }
 
+  // Display bot collection
   function botsCollection() {
     setCollectionVisible(true);
   }
 
+  // Render bot army and either bot collection or bot specs based on visibility
   return (
     <div>
       <YourBotArmy
